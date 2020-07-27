0f50f8a4228d0f5cf43704e7a3770efcc6b5151c
lakejason0 <sunliyuan200402@outlook.com>
2020-07-27 11:53:23 +0800

    Update

--- src/components/getThreadForm.vue
+++ src/components/getThreadForm.vue
@@ -51,7 +51,7 @@
     </v-card-text>
     <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="primary">Login</v-btn>
+        <v-btn text color="primary">Login</v-btn>
     </v-card-actions>
     </v-card>
 </v-col>
