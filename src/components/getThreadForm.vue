e4d22bf34bd2859d105e00713b46358fc1614f6c
LittleC <i@ltlec.cn>
2020-08-10 14:59:30 +0800

    :sparkles: 骨架 文章不存在提示 表单验证

--- src/components/getThreadForm.vue
+++ src/components/getThreadForm.vue
@@ -4,23 +4,25 @@
       <v-toolbar-title>{{ $t('getThreadForm.title') }}</v-toolbar-title>
       <v-spacer></v-spacer>
     </v-toolbar>
-    <v-card-text>
-      <v-form ref="form">
+    <v-form ref="form" v-model="validated">
+      <v-card-text>
         <v-text-field
-          :label="$t('getThreadForm.threadUID')"
-          :counter="max"
-          :rules="rules"
-          name="thread-uid"
-          prepend-icon="mdi-forum"
-          type="text"
-          v-model="threadUID"
+            :counter="max"
+            :label="$t('getThreadForm.threadUID')"
+            :rules="rules"
+            name="thread-uid"
+            prepend-icon="mdi-forum"
+            type="text"
+            v-model="threadUID"
         ></v-text-field>
-      </v-form>
-    </v-card-text>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn text :color="$vuetify.theme.dark ? 'dark' : 'primary'" @click="goToThread(threadUID)">{{$t('getThreadForm.goButton')}}</v-btn>
-    </v-card-actions>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn :color="$vuetify.theme.dark ? 'dark' : 'primary'" :disabled="!validated" @click="goToThread(threadUID)"
+               text>{{ $t('getThreadForm.goButton') }}
+        </v-btn>
+      </v-card-actions>
+    </v-form>
   </v-card>
 </template>
 
@@ -30,26 +32,24 @@ import router from "../router";
 export default {
   data: () => ({
     threadUID: "",
-    max: 30
+    max: 30,
+    validated: false
   }),
   computed: {
-    rules () {
+    rules() {
       const rules = []
       if (this.max) {
-        const rule = v => (v || '').length <= this.max || this.$t('getThreadForm.validateMaxError', {max: this.max})
+        let rule = v => (v || '').length <= this.max || this.$t('getThreadForm.validateMaxError', {max: this.max})
         rules.push(rule)
       }
+      rules.push(v => v.length !== 0 || "输入 请")// @TODO i18n
       return rules;
+
     }
   },
-  watch: {
-    max: 'validateField',
-    threadUID: 'validateField'
-  },
   methods: {
-    goToThread: (threadUID) => { router.push(`thread/${threadUID}`) },
-    validateField () {
-      this.$refs.form.validate()
+    goToThread: (threadUID) => {
+      router.push(`thread/${threadUID}`)
     }
   }
 };
--- src/views/Thread.vue
+++ src/views/Thread.vue
@@ -1,60 +1,91 @@
 <template>
+  <div>
+    <v-row align="center"
+           justify="center" v-if="loading">
+      <v-col
+          cols="12"
+          md="8"
+          sm="8"
+      >
+        <h1 class="text-h2"><v-skeleton-loader
+            type="heading"
+            class="my-4"
+        ></v-skeleton-loader></h1>
+      </v-col>
+      <v-col
+          cols="12"
+          md="8"
+          sm="8"
+      >
+        <h2 class="text-h6"><v-skeleton-loader
+            type="heading"
+            class="mx-auto"
+        ></v-skeleton-loader></h2>
+      </v-col>
+    </v-row>
     <v-row
         align="center"
         justify="center"
+        v-else-if="success && !loading"
     >
-        <v-col
-            cols="12"
-            sm="8"
-            md="8"
-        >
-            <h1 class="text-h2">{{ threadData.thread.title }}</h1>
-        </v-col>
-        <v-col
-            cols="12"
-            sm="8"
-            md="8"
-        >
-            <h2 class="text-h6">{{ threadData.thread.thread }}</h2>
-        </v-col>
-        <v-col
-            cols="12"
-            sm="8"
-            md="8"
-            v-for="floorData in threadData.posts"
-            :key="floorData.floor"
-        >
-            <thread-card :floor-data="floorData"/>
-        </v-col>
+      <v-col
+          cols="12"
+          md="8"
+          sm="8"
+      >
+        <h1 class="text-h2">{{ threadData.thread.title }}</h1>
+      </v-col>
+      <v-col
+          cols="12"
+          md="8"
+          sm="8"
+      >
+        <h2 class="text-h6">{{ threadData.thread.thread }}</h2>
+      </v-col>
     </v-row>
+    <v-row v-else align="center"
+           justify="center">
+      <v-alert type="error">
+        <!-- // @TODO i18n -->
+        不存在!
+      </v-alert>
+    </v-row>
+  </div>
 </template>
 
 <script>
 import threadCard from "@/components/threadCard.vue";
 
 export default {
-    name: "Thread",
-    components: {
-        threadCard
-    },
-    data: () => ({
-        threadData: {title: "Loading...", thread: "Loading...", posts: []}
-    }),
-    async mounted() {
-        let response = await this.getThreadData(this.$route.params.threadUID)
-        this.threadData = response.data.data
-        console.log(this.threadData)
-    },
-    methods: {
-      getThreadData(threadUID) { 
-        return this.$http.post(`thread/${threadUID}`, {
-          action: "get",
-          data: {thread: threadUID}
-        })
-      },
-      makeToast: (toastData) => {
-        return toastData;
-      }
+  name: "Thread",
+  components: {
+    threadCard
+  },
+  data: () => ({
+    threadData: {title: "Loading...", thread: "Loading...", posts: []},
+    loading: true,
+    success: false
+  }),
+  async mounted() {
+    let response = await this.getThreadData(this.$route.params.threadUID)
+
+    this.loading = false
+    if (response.data.code === 200) {
+      this.threadData = response.data.data
+      this.success = true
     }
+    console.log(this.threadData)
+  },
+  methods: {
+    getThreadData(threadUID) {
+      return this.$http.post(`thread/${threadUID}`, {
+        action: "get",
+        data: {thread: threadUID}
+      })
+    },
+    makeToast: (toastData) => {
+      return toastData;
+    }
+  }
 }
 </script>
