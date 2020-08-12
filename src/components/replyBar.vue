fca7b2912eda1b00ae82794e7e25ba88e235c2d3
lakejason0 <sunliyuan200402@outlook.com>
2020-08-12 11:59:24 +0800

    Update

--- src/components/replyBar.vue
+++ src/components/replyBar.vue
@@ -5,13 +5,18 @@
           dense
           floating
           @click="$emit('click')"
+          fixed
+          scroll-off-screen
+          class="reply-bar"
+          :class="{ 'reply-bar--hidden': !showReplyBar }"
       >
         <v-text-field
             hide-details
             single-line
-            disabled
+            readonly
+            :value="content"
         ></v-text-field>
-        <v-btn icon disable>
+        <v-btn icon>
           <v-icon>send</v-icon>
         </v-btn>
       </v-toolbar>
@@ -19,6 +24,47 @@
   </div>
 </template>
 
+<style scoped>
+.reply-bar {
+    transform: translate3d(0, 0, 0);
+    transition: 0.1s all ease-out
+}
+
+.reply-bar--hidden {
+    box-shadow: none;
+    transform: translate3d(0, 200%, 0);
+}
+</style>
+
 <script>
-export default {}
+export default {
+    props: {
+        content: String
+    },
+    data: () => ({
+        showReplyBar: true,
+        lastScrollPosition: 0
+    }),
+    mounted () {
+        window.addEventListener('scroll', this.onScroll)
+    },
+    beforeDestroy () {
+        window.removeEventListener('scroll', this.onScroll)
+    },
+    methods: {
+        onScroll () {
+            const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop
+            if (currentScrollPosition < 0) {
+                return
+            }
+
+            if (Math.abs(currentScrollPosition - this.lastScrollPosition) < 60 ) {
+                return
+            }
+
+            this.showReplyBar = currentScrollPosition < this.lastScrollPosition
+            this.lastScrollPosition = currentScrollPosition
+        }
+    }
+}
 </script>
--- src/components/replyThreadForm.vue
+++ src/components/replyThreadForm.vue
@@ -2,26 +2,14 @@
   <div class="text-center">
     <v-bottom-sheet v-model="sheet" inset>
       <template v-slot:activator="{}">
-        <reply-bar 
-          @click="sheet = !sheet"
-        />
+        <reply-bar @click="sheet = !sheet" :content="content" />
       </template>
-      <v-sheet class="text-center">
-        <v-btn
-          class="mt-6"
-          text
-          color="accent"
-          @click="sheet = !sheet"
-        >close</v-btn>
-        <v-container>
-            <v-form>
-                <v-row>
-                    <v-col
-                        cols="12"
-                        sm="4"
-                        md="4"
-                    >
-                        <v-text-field
+          <v-form>
+            <v-card tile>
+                <v-card-text>
+                        <v-row>
+                        <v-col cols="12" sm="4" md="4">
+                            <v-text-field
                             :counter="max"
                             :label="$t('replyThreadForm.username')"
                             :rules="rules"
@@ -29,25 +17,33 @@
                             prepend-icon="mdi-account"
                             type="text"
                             v-model="username"
-                        ></v-text-field>
-                    </v-col>
-                    <v-col
-                        cols="12"
-                        sm="12"
-                        md="12"
-                    >
-                        <v-textarea
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="12" md="12">
+                            <v-textarea
                             v-model="content"
                             prepend-icon="mdi-forum"
                             :label="$t('replyThreadForm.content')"
-                            autogrow
+                            auto-grow
                             rows="4"
-                        ></v-textarea>
-                    </v-col>
-                </v-row>
-            </v-form>
-        </v-container>
-      </v-sheet>
+                            ></v-textarea>
+                        </v-col>
+                        </v-row>
+                        
+                </v-card-text>
+                <v-card-actions>
+                <v-spacer></v-spacer>
+                    <v-btn
+                        class="mt-6"
+                        text
+                        color="accent"
+                        @click="sheet = !sheet"
+                    >
+                        {{$t('replyThreadForm.closeButton')}}
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+          </v-form>
     </v-bottom-sheet>
   </div>
 </template>
@@ -56,28 +52,30 @@
 import replyBar from "@/components/replyBar.vue";
 
 export default {
-    components: {
-        replyBar
-    },
-    data: () => ({
-        sheet: false,
-        max: 25,
-        username: "",
-        content: "",
-    }),
-    computed: {
-        rules() {
-            const rules = []
-            if (this.max) {
-                let rule = v => (v || '').length <= this.max || this.$t('replyThreadForm.validateMaxError', {max: this.max})
-                rules.push(rule)
-            }
-            rules.push(v => v.length !== 0 || this.$t('replyThreadForm.validateNullError') )// @TODO i18n
-            return rules;
-        }
-    },
-    methods: {
-
+  components: {
+    replyBar
+  },
+  data: () => ({
+    sheet: false,
+    max: 25,
+    username: "",
+    content: ""
+  }),
+  computed: {
+    rules() {
+      const rules = [];
+      if (this.max) {
+        let rule = v =>
+          (v || "").length <= this.max ||
+          this.$t("replyThreadForm.validateMaxError", { max: this.max });
+        rules.push(rule);
+      }
+      rules.push(
+        v => v.length !== 0 || this.$t("replyThreadForm.validateNullError")
+      ); // @TODO i18n
+      return rules;
     }
-}
+  },
+  methods: {}
+};
 </script>
