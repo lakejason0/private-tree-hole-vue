3791c3b7c52fc6f673e9f1ecbd4a253e901f1be7
LittleC <i@ltlec.cn>
2020-08-12 13:19:17 +0800

    隐藏reply bar

--- src/components/replyBar.vue
+++ src/components/replyBar.vue
@@ -1,26 +1,27 @@
 <template>
-  <div style="position: fixed; bottom: 16px; left: 0; right: 0;display: flex;align-items: center;justify-content: center">
-    <div>
-      <v-toolbar
-          dense
-          floating
-          @click="$emit('click')"
-          fixed
-          class="reply-bar"
-          :class="{ 'reply-bar--hidden': !showReplyBar }"
-      >
-        <v-text-field
-            hide-details
-            single-line
-            readonly
-            :value="content"
-        ></v-text-field>
-        <v-btn icon>
-          <v-icon>send</v-icon>
-        </v-btn>
-      </v-toolbar>
+    <div
+        style="position: fixed; bottom: 16px; left: 0; right: 0;display: flex;align-items: center;justify-content: center">
+        <div ref="bar">
+            <v-toolbar
+                :class="{ 'reply-bar--hidden': !showReplyBar }"
+                @click="$emit('click')"
+                class="reply-bar"
+                dense
+                fixed
+                floating
+            >
+                <v-text-field
+                    :value="content"
+                    hide-details
+                    readonly
+                    single-line
+                ></v-text-field>
+                <v-btn icon>
+                    <v-icon>send</v-icon>
+                </v-btn>
+            </v-toolbar>
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
@@ -36,37 +37,42 @@
 </style>
 
 <script>
+import {throttle} from 'lodash'
 export default {
     props: {
         content: String
     },
     data: () => ({
         showReplyBar: true,
-        lastScrollPosition: 0
+        lastScrollPosition: 0,
+        barHeight: 0,
+        _onScroll: null
     }),
-    mounted () {
-        window.addEventListener('scroll', this.onScroll)
+    beforeMount() {
+        this._onScroll = throttle(this.onScroll, 20)
     },
-    beforeDestroy () {
-        window.removeEventListener('scroll', this.onScroll)
+    mounted() {
+        window.addEventListener('scroll', this._onScroll)
+    },
+    beforeDestroy() {
+        window.removeEventListener('scroll', this._onScroll)
     },
     methods: {
-        onScroll () {
-            const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop
-            var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-            var windowHeight = document.documentElement.clientHeight || document.body.clientHeight;
-            var scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
-
-            if (currentScrollPosition < 0) {
-                return
+        onScroll() {
+            if(!this.barHeight){
+                this.barHeight = this.$refs.bar.getBoundingClientRect().height
             }
-
-            if (Math.abs(currentScrollPosition - this.lastScrollPosition) < 60 ) {
-                return
+            let st = window.pageYOffset || document.documentElement.scrollTop;
+            if (st > this.lastScrollTop) {
+                this.showReplyBar = false
+                let bottom = st > document.body.scrollHeight - this.barHeight - window.innerHeight
+                if (bottom) {
+                    this.showReplyBar = true
+                }
+            } else {
+                this.showReplyBar = true
             }
-
-            this.showReplyBar = (currentScrollPosition < this.lastScrollPosition) || (scrollTop + windowHeight >= scrollHeight)
-            this.lastScrollPosition = currentScrollPosition
+            this.lastScrollTop = st <= 0 ? 0 : st;
         }
     }
 }
