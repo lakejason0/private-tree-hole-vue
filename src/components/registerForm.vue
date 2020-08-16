bad1427c2658d6a9a1bd8208b7ba59458c747adc
lakejason0 <sunliyuan200402@outlook.com>
2020-08-17 01:14:19 +0800

    Update

--- src/components/registerForm.vue
+++ src/components/registerForm.vue
@@ -1,10 +1,10 @@
 <template>
   <v-card class="elevation-1">
     <v-toolbar :color="$vuetify.theme.dark ? 'dark' : 'primary'" dark flat>
-      <v-toolbar-title>{{ $t("loginForm.title") }}</v-toolbar-title>
+      <v-toolbar-title>{{ $t("registerForm.title") }}</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon large @click="$emit('toggle-register')">
-        <v-icon>mdi-account-plus</v-icon>
+      <v-btn icon large @click="$emit('toggle-login')">
+        <v-icon>mdi-login</v-icon>
       </v-btn>
     </v-toolbar>
 
@@ -12,7 +12,7 @@
       <v-card-text>
         <v-text-field
           :counter="usernameMax"
-          :label="$t('loginForm.account')"
+          :label="$t('registerForm.account')"
           :rules="usernameRules"
           name="account"
           autocomplete="username"
@@ -22,7 +22,17 @@
         >
         </v-text-field>
         <v-text-field
-          :label="$t('loginForm.password')"
+          :label="$t('registerForm.email')"
+          :rules="emailRules"
+          name="email"
+          prepend-icon="mdi-email"
+          type="email"
+          autocomplete="email"
+          v-model="email"
+        >
+        </v-text-field>
+        <v-text-field
+          :label="$t('registerForm.password')"
           :rules="passwordRules"
           name="password"
           prepend-icon="mdi-lock"
@@ -36,7 +46,7 @@
       <v-card-actions>
         <v-spacer></v-spacer>
         <v-btn :disabled="!validated" text :color="$vuetify.theme.dark ? 'dark' : 'primary'">{{
-          $t("loginForm.loginButton")
+          $t("registerForm.registerButton")
         }}</v-btn>
       </v-card-actions>
     </v-form>
@@ -50,6 +60,7 @@ export default {
     validated: false,
     usernameMax: 25,
     username: "",
+    email: "",
     password: "",
     showPassword: false
   }),
@@ -59,10 +70,10 @@ export default {
       if (this.usernameMax) {
         let rule = v =>
           (v || "").length <= this.usernameMax ||
-          this.$t("loginForm.validateUsernameMaxError", { max: this.usernameMax });
+          this.$t("registerForm.validateUsernameMaxError", { max: this.usernameMax });
         rules.push(rule);
       }
-      rules.push(v => v.length !== 0 || this.$t("loginForm.validateUsernameNullError"));
+      rules.push(v => v.length !== 0 || this.$t("registerForm.validateUsernameNullError"));
       return rules;
     },
     passwordRules() {
@@ -70,10 +81,17 @@ export default {
       if (this.passwordMax) {
         let rule = v =>
           (v || "").length <= this.passwordMax ||
-          this.$t("loginForm.validatePasswordMaxError", { max: this.passwordMax });
+          this.$t("registerForm.validatePasswordMaxError", { max: this.passwordMax });
         rules.push(rule);
       }
-      rules.push(v => v.length !== 0 || this.$t("loginForm.validatePasswordNullError"));
+      rules.push(v => v.length !== 0 || this.$t("registerForm.validatePasswordNullError"));
+      return rules;
+    },
+    emailRules() {
+      const rules = [];
+      rules.push(
+        v => !v || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || this.$t("registerForm.validateEmailError")
+      )
       return rules;
     }
   }
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="user">
     <v-row align="center" justify="center">
-      <v-col cols="12" sm="8" md="4">
-        <login-form />
+      <v-col cols="12" sm="8" md="4" >
+        <login-form v-if="toggle === 'login'" @toggle-register="toggleRegister"/>
+        <register-form v-if="toggle === 'register'" @toggle-login="toggleLogin"/>
       </v-col>
     </v-row>
   </div>
@@ -10,9 +11,26 @@
 
 <script>
 import loginForm from "@/components/loginForm.vue";
+import registerForm from "@/components/registerForm.vue";
 export default {
+  mounted() {
+    this.$on("toggle-register", this.toggleRegister);
+    this.$on("toggle-login", this.toggleLogin);
+  },
+  data: () => ({
+    toggle: "register"
+  }),
   components: {
-    loginForm
+    loginForm,
+    registerForm
+  },
+  methods: {
+    toggleRegister() {
+      this.toggle = "register"
+    },
+    toggleLogin() {
+      this.toggle = "login"
+    }
   }
 };
 </script>
