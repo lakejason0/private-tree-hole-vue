b2b5315400f8a07728c9e466456911d8d2f72e8e
lakejason0 <sunliyuan200402@outlook.com>
2020-08-23 00:02:27 +0800

    Update

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ import router from "./router";
 import i18n from "./i18n";
 
 Vue.config.productionTip = false;
+
 let httpClient = axios.create({
   baseURL: process.env.TREE_HOLE_API_BASE_URL
 });
