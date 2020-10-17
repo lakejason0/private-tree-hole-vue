836fe7f4bd242bf0de2e9fffad796d3385cc8c83
lakejason0 <sunliyuan200402@outlook.com>
2020-10-17 20:42:06 +0800

    Update

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,7 +1,26 @@
 <template>
   <div class="user">
+    <v-row align="center" justify="center" v-if="loading">
+      <v-col cols="12" md="8" sm="8">
+        <h1 class="text-h2">
+          <v-skeleton-loader type="heading" class="my-4"></v-skeleton-loader>
+        </h1>
+      </v-col>
+      <v-col cols="12" md="8" sm="8">
+        <h2 class="text-h6">
+          <v-skeleton-loader type="heading" class="mx-auto"></v-skeleton-loader>
+        </h2>
+      </v-col>
+    </v-row>
+    <v-row align="center" justify="center" v-if="!loading && logged_in">
+      <v-col cols="12" md="8" sm="8">
+        <h1 class="text-h2">
+          {{ $t("user.title") }}
+        </h1>
+      </v-col>
+    </v-row>
     <v-row align="center" justify="center">
-      <v-col cols="12" sm="8" md="4">
+      <v-col cols="12" sm="8" md="4" >
         <login-form
           v-if="toggle === 'login'"
           @toggle-register="toggleRegister"
@@ -25,15 +44,19 @@ export default {
 
     this.$http("/user/info").then(res => {
       if (res.status === 200) {
-        this.$dialog.message.info(`欢迎 ${res.data.data.username}`, {
+        this.$dialog.message.info(this.$t("user.loggedInToast",{username: res.data.data.username}), {
           position: "bottom-left",
           icon: true
         });
+        this.logged_in = true
       }
+      this.loading = false
     });
   },
   data: () => ({
-    toggle: "register"
+    toggle: "login",
+    loading: true,
+    logged_in: null,
   }),
   components: {
     loginForm,
