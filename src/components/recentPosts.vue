03a5d4c242d0b7e51d0fe92e89d68e639d862300
lakejason0 <sunliyuan200402@outlook.com>
2020-10-25 16:21:13 +0800

    Update

---
+++ src/components/recentPosts.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-row align="center" justify="center">
+        <v-col cols="12" md="8" sm="8">
+            <thread-card />
+        </v-col>
+    </v-row>
+  </div>
+</template>
+<script>
+import threadCard from "@/components/threadCard.vue"
+export default {
+    components: {
+        threadCard
+    }
+}
+</script>
--- src/views/User.vue
+++ src/views/User.vue
@@ -18,6 +18,34 @@
           {{ $t("user.title") }}
         </h1>
       </v-col>
+      <v-col cols="12" md="8" sm="8">
+        <v-tabs
+          v-model="tab"
+          background-color="transparent"
+        >
+          <v-tabs-slider></v-tabs-slider>
+          <v-tab href="#tab-recent">
+            {{ $t('user.tabs.recent') }}
+          </v-tab>
+          <v-tab href="#tab-preferences">
+            {{ $t('user.tabs.preferences') }}
+          </v-tab>
+          <v-tab href="#tab-query">
+            {{ $t('user.tabs.query') }}
+          </v-tab>
+        </v-tabs>
+        <v-tabs-items v-model="tab">
+          <v-tab-item value="tab-recent">
+            <recent-posts></recent-posts>
+          </v-tab-item>
+          <v-tab-item value="tab-preferences">
+            <preferences></preferences>
+          </v-tab-item>
+          <v-tab-item value="tab-query">
+            <query></query>
+          </v-tab-item>
+        </v-tabs-items>
+      </v-col>
     </v-row>
     <v-row align="center" justify="center" v-else>
       <v-col cols="12" sm="8" md="4" >
@@ -37,6 +65,9 @@
 <script>
 import loginForm from "@/components/loginForm.vue";
 import registerForm from "@/components/registerForm.vue";
+import recentPosts from "@/components/recentPosts.vue";
+import preferences from "@/components/preferences.vue";
+import query from "@/components/query.vue";
 export default {
   mounted() {
     this.$on("toggle-register", this.toggleRegister);
@@ -71,10 +102,14 @@ export default {
     toggle: "login",
     loading: true,
     logged_in: null,
+    tab: null,
   }),
   components: {
     loginForm,
-    registerForm
+    registerForm,
+    recentPosts,
+    preferences,
+    query,
   },
   methods: {
     toggleRegister() {
